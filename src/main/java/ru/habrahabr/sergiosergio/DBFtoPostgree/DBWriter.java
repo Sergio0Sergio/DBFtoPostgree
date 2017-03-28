2038f717ea0b557e38c6992748806e4efe0a5a5e
Sergei Grimanov <tosergi@gmail.com>
2017-03-28 22:13:53 +0300

    Переработал методы read() в StringsInputStream.

--- src/main/java/ru/habrahabr/sergiosergio/DBFtoPostgree/DBWriter.java
+++ src/main/java/ru/habrahabr/sergiosergio/DBFtoPostgree/DBWriter.java
@@ -8,50 +8,46 @@ import org.postgresql.copy.CopyManager;
 
 public class DBWriter extends Thread {
 
-    private StringsInputStream inputStream;
-    private CopyManager copyManager;
-    private String sqlVariables;
-    private String tableName;
-    private Connection connection;
+	private StringsInputStream inputStream;
+	private CopyManager copyManager;
+	private String sqlVariables;
+	private String tableName;
+	private Connection connection;
 
-    public DBWriter(StringsInputStream inputStream, CopyManager copyManager, String sqlVariables, String tableName,
-	    Connection connection) {
+	public DBWriter(StringsInputStream inputStream, CopyManager copyManager, String sqlVariables, String tableName, Connection connection) {
 
-	this.inputStream = inputStream;
-	this.copyManager = copyManager;
-	this.sqlVariables = sqlVariables;
-	this.tableName = tableName;
-	this.connection = connection;
+		this.inputStream = inputStream;
+		this.copyManager = copyManager;
+		this.sqlVariables = sqlVariables;
+		this.tableName = tableName;
+		this.connection = connection;
 
-    }
-
-    @Override
-    public void run() {
-
-	try {
-	    System.out.println("Начало записи в базу.");
-	    copyManager.copyIn(
-		    "COPY \"" + tableName + "\"(" + sqlVariables
-			    + ")FROM STDIN	WITH (FORMAT CSV, DELIMITER ',', HEADER FALSE, QUOTE '\"', ESCAPE '\"', ENCODING 'UTF-8');",
-		    inputStream);
-	} catch (SQLException e) {
-	    System.err.println("Ошибка записи в базу.");
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    System.err.println("Ошибка записи в базу.");
-	    e.printStackTrace();
 	}
 
-	try {
-	    connection.close();
-	} catch (SQLException e) {
-	    System.err.println("Ошибка закрытия базы.");
-	    e.printStackTrace();
+	@Override
+	public void run() {
+
+		try {
+			System.out.println("Начало записи в базу.");
+			copyManager.copyIn("COPY \"" + tableName + "\"(" + sqlVariables + ")FROM STDIN WITH (FORMAT CSV, DELIMITER ',', HEADER FALSE, QUOTE '\"', ESCAPE '\"', ENCODING 'UTF-8');", inputStream);
+		} catch (SQLException e) {
+			System.err.println("Ошибка записи в базу.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Ошибка записи в базу.");
+			e.printStackTrace();
+		}
+
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			System.err.println("Ошибка закрытия базы.");
+			e.printStackTrace();
+		}
+
+		System.out.println("Запись завершена.");
+		System.out.println("Работа программы завершена успешно.");
+
 	}
-
-	System.out.println("Запись завершена.");
-	System.out.println("Работа программы завершена успешно.");
-
-    }
 
 }
